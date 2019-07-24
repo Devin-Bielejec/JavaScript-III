2ea251cf9dbab2739082f98b1d15aeef016ab6eb
devin-bielejec <devin.bieleje@gmail.com>
2019-07-24 15:10:48 -0400

    Finished double checking the file name this

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. Window Binding - Using this helps refer people to the window, which is like the place where this belongs to. If "this" is not inside a function, "this" is the window [think globally] - "this" binds to the global, which in the case of a window is the window.
+* 1. Window Binding - Using "this" helps refer people to the window, which is the environment where "this" belongs to initially. If "this" is not inside of anything, "this" is the window [think globally] - "this" binds to the global, which in the case of a window is the window.
 
-* 2. Implicit Binding - When "this" is used within an object "this" refers to the object name. Look to the left of the dot when the function is invoked to see what "this" is referring to
+* 2. Implicit Binding - When "this" is used within an object "this" refers to the object name and properties. Look to the left of the dot when the function is invoked to see what "this" is referring to.
 
-* 3. New Binding - When we create a new binding using bind, in which "this" transfers its owner to the new object specified.
+* 3. New Binding - By using the keyword "new" we can create an new instance of an object but with potentially different arguments, thus creating visually new properties. It will appear that "this" has changed.
 
-* 4. Explicit Binding - When you use apply or call to switch where "this" belongs.
+* 4. Explicit Binding - When you use apply or call to switch where "this" belongs. On every function there is an option to use call/apply, which allows us to take data from an obj and then within our function we can refer to that data by using this
 *
 * write out a code example of each explanation above
 */
@@ -15,10 +15,12 @@
 // Principle 1
 
 // code example for Window Binding - If we were in a window
-const a = 42;
-console.log(global.a);
+a = 42;
+console.log(global.a); //or console.log(window.a);
 
 // Principle 2
+
+
 
 // code example for Implicit Binding
 const devin = {
@@ -45,7 +47,7 @@ console.log(sadPerson.feeling);
 
 const hatefulPerson = new superCoolPerson("hateful");
 console.log(hatefulPerson.feeling);
-//but now this.feeling refers to hate because we rebound it!
+//but now this.feeling refers to hateful because we rebound it!
 
 
 
@@ -57,11 +59,14 @@ const newAttributes = {
     feeling: "excitement"
 }
 
-function showMeLove (){
+const oldAttributes = {
+    feeling: "despair"
+}
+
+//On every function there is an option to use call/apply, which allows us to take data from an obj and then within our function we can refer to that data by using this
+function showMe (){
     console.log(`Hello ${this.feeling} is very awesome!`);
 }
 
-showMeLove.call(newAttributes);
-//So we create a function.
-
-//On every function there is an option to use call/apply, which allows us to take data from an obj and then within our function we can refer to that data by using this
+showMe.call(newAttributes);
+showMe.call(oldAttributes);
