1510cf64d7afb52865c4f7fac16febcc8c68e7ee
devin-bielejec <devin.bieleje@gmail.com>
2019-07-24 14:15:26 -0400

    Finished implicit binding

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. Window Binding - Using this helps refer people to the window, which is like the place where this belongs to. If "this" is not inside a function, "this" is the window [think globally] - "this" binds to the global, which in the case of a window is the window.
 
-* 2. Implicit Binding - When "this" is used within an object "this" refers to the object name.
+* 2. Implicit Binding - When "this" is used within an object "this" refers to the object name. Look to the left of the dot when the function is invoked to see what "this" is referring to
 
 
 * 3. New Binding - When 
@@ -18,13 +18,17 @@
 const a = 42;
 console.log(global.a);
 
-
-
-
 // Principle 2
 
 // code example for Implicit Binding
+const devin = {
+    name: "Devin",
+    eat: function(){
+        console.log(`${this.name} likes to eat`);
+    }
+};
 
+devin.eat();
 
 
 // Principle 3
