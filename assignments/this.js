4f6845cd8ec32f828eb755bfc733df451ba4e55e
devin-bielejec <devin.bieleje@gmail.com>
2019-07-24 14:10:42 -0400

    finished window binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,21 +1,31 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - Using this helps refer people to the window, which is like the place where this belongs to. If "this" is not inside a function, "this" is the window [think globally] - "this" binds to the global, which in the case of a window is the window.
+
+* 2. Implicit Binding - When "this" is used within an object "this" refers to the object name.
+
+
+* 3. New Binding - When 
+* 4. Explicit Binding -
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+// code example for Window Binding - If we were in a window
+const a = 42;
+console.log(global.a);
+
+
+
 
 // Principle 2
 
 // code example for Implicit Binding
+
+
 
 // Principle 3
 
